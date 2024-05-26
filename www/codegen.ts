19b6321e2d20ac55de438350fba71cb4ed5cb743
Colin <colin.vandervoort.42@gmail.com>
2024-05-26 06:40:31 -0700

    add launch config for debugging graphql typescript codegen

--- www/codegen.ts
+++ www/codegen.ts
@@ -1,13 +1,17 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
 const endpoint = process.env.GRAPHQL_ENDPOINT || 'http://localhost:8080/v1/graphql'
+const hasuraAdminSecret = process.env.HASURA_GRAPHQL_ADMIN_SECRET
+if (!hasuraAdminSecret) {
+  throw new Error('HASURA_GRAPHQL_ADMIN_SECRET is required')
+}
 
 const config: CodegenConfig = {
   schema: [
     {
       [endpoint]: {
         headers: {
-          'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
+          'x-hasura-admin-secret': hasuraAdminSecret
         }
       },
     },
@@ -15,7 +19,7 @@ const config: CodegenConfig = {
   documents: [
     'app/**/*.graphql',
   ],
-  ignoreNoDocuments: true, // for better experience with the watcher
+  ignoreNoDocuments: false,
   generates: {
     './app/graphql/_generated.ts': {
       plugins: [
